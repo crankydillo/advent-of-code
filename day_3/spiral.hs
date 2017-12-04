17b321e5c3bb8d8f96e9b0ffb451213a0d650205
Samuel Cox <sacox@paypal.com>
2017-12-03 22:32:06 -0600

    Wow, I know I must have missed something...  Even so, pretty cool coming up with the spiral as a list of coordinates.

---
+++ day_3/spiral.hs
@@ -0,0 +1,81 @@
+-- Surely there's a much more efficient way to do this, but I already
+-- have 2 side projects going!  Ok, enough with excuses...
+import Data.List
+import Data.Maybe
+import qualified Data.Map as M
+
+numbersInSpiral = [ (x * 2 + 1)^2 | x <- [0..] ]
+
+spiralContains x = fromJust $ findIndex (x <=) numbersInSpiral
+
+start 0 = 1
+start spiral = ((numbersInSpiral !! spiral - numbersInSpiral !! (spiral - 1)) `div` 2) + numbersInSpiral !! (spiral - 1)
+
+matrixSize spirals = spirals * 2 + 1
+
+centerPoint matrixSize = (x, x) where x = (matrixSize `div` 2)
+
+position x
+      | x == startPt                   = (0, 0)
+      | x < startPt && x >= rightUpper = (x - startPt, startPt - rightUpper)
+      | x < startPt                    = (rightUpper - x, startPt - rightUpper)
+      | x > startPt && x <= leftLower  = (x - startPt, 0)
+      | x > startPt                    = (leftLower - startPt, x - leftLower)
+      where spiral     = spiralContains x
+            startPt    = start spiral
+            matrixS    = matrixSize spiral
+            rightUpper = startPt - matrixS + 1
+            leftLower  = startPt + matrixS - 1
+
+ans square = abs (px - cx) + abs (py - cy)
+      where spiral     = spiralContains square
+            matrixS    = matrixSize spiral
+            (px, py)   = position square
+            (cx, cy)   = centerPoint matrixS
+
+main = do putStrLn $ show $ numbersInSpiral !! 3
+          putStrLn $ show $ ans 1024  -- this is wrong
+          putStrLn $ show $ ans 277678 -- but this is right???
+          putStrLn $ show $ lookup (2,2) myMap
+          --putStrLn $ show $ myMap
+          putStrLn $ show $ spiralCoords 3
+          putStrLn "--------"
+          putStrLn $ show $ spiralCoords 9 
+          putStrLn "--------"
+          putStrLn $ show $ spiralCoords 5
+          putStrLn "--------"
+          putStrLn $ show $ head $ dropWhile (<= 277678) $ sort (map snd (spiralVals 33))
+          putStrLn "--------"
+          putStrLn $ show $ spiralVals 5
+
+
+myMap = map (\x -> (x, 1)) (spiralCoords 5)
+
+spiralVals s = let first = ((centerPoint s), 1)
+               in first : (spiralValsH  (tail $ spiralCoords s) [first])
+
+spiralValsH :: Integral i => [(i, i)] -> [((i, i), i)] -> [((i, i), i)]
+spiralValsH [] _ = []
+spiralValsH coords prev =
+    let next = ((head coords), inc (head coords) prev)
+    in next : (spiralValsH (tail coords) (prev ++ [next]))
+
+inc :: Integral i => (i, i) -> [((i, i), i)] -> i
+inc (x, y) previous = foldl (\acc p -> acc + (lup p)) 0 $ adjacents (x, y)
+           where lup p = case (lookup p previous) of
+                           Just x  -> x
+                           Nothing -> 0
+
+adjacents (x, y) = [(x-1, y), (x, y-1), (x-1, y-1), (x+1, y), (x, y+1), (x+1, y+1), (x+1, y-1), (x-1, y+1) ]
+
+-- Should redo the first question on top of this
+spiralCoords s = centerPoint s : reverse (spiralCoordsH s s 1)
+
+spiralCoordsH :: (Integral i) => i -> i -> i -> [(i, i)]
+spiralCoordsH 1 _ _ = []
+spiralCoordsH s origS ctr = [(origS-ctr,y) | y <- reverse [ctr-1..origS-ctr]] ++
+                             [(x-1,  ctr-1) | x <- reverse [ctr..origS-ctr]] ++
+                             [(ctr-1, y) | y <- [ctr..(origS - (ctr + 1))]] ++
+                             --[(x,  s-1) | x <- [ctr-1..(s `div` 2)]] ++
+                             [(x,  origS - ctr) | x <- [ctr-1..(origS - (ctr + 1))]] ++
+                             spiralCoordsH (s - 2) origS (ctr + 1)
